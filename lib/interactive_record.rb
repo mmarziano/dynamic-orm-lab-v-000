defbfacbfb421bd6c70d3b771aed3e4976fe443f
Monica Marziano <mmarziano1@gmail.com>
2019-02-18 12:35:28 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,7 @@ class InteractiveRecord
   end  
   
   def self.find_by(attribute)
-    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute[:k]} = #{attribute.value_at}"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute[:k]} = #{attribute.values_at(:k)}"
     
     DB[:conn].execute(sql)
   end 
